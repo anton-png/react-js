8add4eb908413ed27c3dda1c7cb224cadde9ab88
anton-png <anton29071981@mail.ru>
2020-08-09 11:44:35 +0400

    Updates

---
+++ precache-manifest.381546f2bfe97b9cfb1218f330402a66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d0473dddd9748de69a35bff59d17c4",
+    "url": "/react-js/index.html"
+  },
+  {
+    "revision": "bd81e00319259a0ae6c7",
+    "url": "/react-js/static/css/main.9834a754.chunk.css"
+  },
+  {
+    "revision": "f9f1467df96402ca1b37",
+    "url": "/react-js/static/js/2.c5944a78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-js/static/js/2.c5944a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd81e00319259a0ae6c7",
+    "url": "/react-js/static/js/main.7a06102f.chunk.js"
+  },
+  {
+    "revision": "b08a117e9c1f05cba709",
+    "url": "/react-js/static/js/runtime-main.8adbbf33.js"
+  },
+  {
+    "revision": "b14b7128593cdd4f54898424617c9c59",
+    "url": "/react-js/static/media/doc.b14b7128.svg"
+  },
+  {
+    "revision": "98aafac3724a76d82e5e46df196c0d7c",
+    "url": "/react-js/static/media/index.98aafac3.svg"
+  }
+]);
